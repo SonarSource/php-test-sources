cb31d757d2b18cc3e8af409bde4f26e761ad0358
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2023-04-25 17:17:43 +0200

    Add Flysystem

--- clean-before-commit.sh
+++ clean-before-commit.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo "Remove unintendent files..."
+
 cd src
+find . -name ".DS_Store" -delete
 find . -name "*.adoc" -delete
 find . -name "*.bmp" -delete
 find . -name "*.csv" -delete
+find . -name "*.doc" -delete
+find . -name "*.docx" -delete
+find . -name "*.gif" -delete
 find . -name "*.ods" -delete
 find . -name "*.md" -delete
 find . -name "*.jpeg" -delete
@@ -14,6 +20,11 @@ find . -name "*.tiff" -delete
 find . -name "*.txt" -delete
 find . -name "*.xls" -delete
 find . -name "*.xlsx" -delete
+find . -name "*.zip" -delete
 
-echo "Big files:"
-find . -size +200k
+echo "Remove unintendent files... Done"
+
+echo "Big files (please review them):"
+find . -size +250k
+
+echo "Done\n"
